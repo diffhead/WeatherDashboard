d03d9746c5f0d247d3e27d776772896d1cd15940
Viktor Smagin <thinlineseverywhere@gmail.com>
2022-02-13 20:37:48 +0300

    Init project styles and use SASS

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var tsify = require("tsify");
-var scss = require('gulp-scss');
+var sass = require('gulp-sass')(require('sass'));
+var concat = require('gulp-concat');
 
 var paths = {
     pages: ["workspace/ts/*.html"],
@@ -16,27 +17,27 @@ gulp.task(
     "default",
     gulp.series(
         gulp.parallel("copy-html"), 
-        gulp.parallel(function () {
-            return browserify({
-                basedir: ".",
-                debug: true,
-                entries: ["workspace/ts/index.ts"],
-                cache: {},
-                packageCache: {},
-            })
-            .plugin(tsify)
-            .bundle()
-            .pipe(source("index.js"))
-            .pipe(gulp.dest("bundle"));
-        }),
-        gulp.parallel(function() {
-            return gulp.src(
-                    "workspace/scss/**/*.scss"
-                ).pipe(scss(
-                    {"bundleExec": true}
-                )
-            )
-            .pipe(gulp.dest("bundle"));
-        })
+        gulp.parallel(
+            function () {
+                return browserify({
+                    basedir: ".",
+                    debug: true,
+                    entries: ["workspace/ts/index.ts"],
+                    cache: {},
+                    packageCache: {},
+                })
+                .plugin(tsify)
+                .bundle()
+                .pipe(source("index.js"))
+                .pipe(gulp.dest("bundle"));
+            },
+            function() {
+                return gulp.src('workspace/scss/index.scss')
+                    .pipe(concat("index.css"))
+                    .pipe(sass().on('error', sass.logError))
+                    .pipe(gulp.dest('bundle')
+                );
+            }
+        )
     )
 );
--- templates/error.tpl
+++ templates/error.tpl
@@ -1,22 +1,33 @@
 <html>
     <head>
         <title>{{ code }} Error</title>
-        <link href="/bundle/erorr.css" rel="stylesheet">
+        <link href="/bundle/index.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <div class="content">
             <div class="error-modal">
-                <div class="error-modal-img">
+                <div class="error-modal-menu">
+                    <button>Home</button>
+                    <button>Expand</button>
+                    <button>ShortIt</button>
+                </div>
+                <div class="error-modal-img error-row">
                     <a href="/">
-                        <img src="/images/logo.webp" alt="Site's main logotype">
+                        <div class="error-modal-img__a">
+                            <img src="/images/logo.webp" alt="Site's main logotype">
+                            <span>Go to homepage</span>
+                        </div>
                     </a>
                 </div>
-                <div class="error-code">{{ code }}</div>
-                <div class="error-message">{{ message }}</div>
-                <div class="error-additional">
-                    <pre>{{ extMessage }}</pre>
-                </div>
+                <div class="error-modal-code error-row">{{ code }}</div>
+                <div class="error-modal-message error-row">{{ message }}</div>
+                
+                {% if extMessage and extMessage != '' %}
+                    <div class="error-modal-additional error-row">
+                        <pre>{{ extMessage }}</pre>
+                    </div>
+                {% endif %}
             </div>
         </div>
     </body>
