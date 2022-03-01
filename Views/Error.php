08d0babfd72ac15d5ee07d89ffcc93cc9b08e766
Viktor Smagin <thinlineseverywhere@gmail.com>
2022-03-01 05:11:08 +0300

    Error view small refactoring

--- Views/Error.php
+++ Views/Error.php
@@ -9,14 +9,14 @@ class Error extends View
     protected string $template = 'templates/error.tpl';
 
     protected array  $params = [
-        'code'    => 0,
-        'message' => '',
-        'entity'  => _APP_ENVIRONMENT_
+        'code'       => 0,
+        'message'    => _APP_EMPTY_STRING_,
+        'extMessage' => _APP_EMPTY_STRING_ 
     ];
 
     protected bool   $templateIsFile = true;
 
-    public function __construct(int $code, string $message = 'Error', string $extMessage = '')
+    public function __construct(int $code, string $message = 'Error', string $extMessage = _APP_EMPTY_STRING_)
     {
         parent::__construct();
 
