f98b3df62a1a4a0c8a09af962db8c19c27e01d59
Viktor Smagin <thinlineseverywhere@gmail.com>
2022-04-27 10:00:48 +0300

    Back-end:     Core\Hook\HookAction           ->  removed old hook files     Core\Hook\HookActionCollection ->  removed old hook files

--- Core/Hook/HookActionCollection.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Core\Hook;
-
-use Core\AbstractCollection;
-
-class HookActionCollection extends AbstractCollection
-{
-    protected static string $collectionItemClass = '\\Core\\Hook\\HookAction';
-}
