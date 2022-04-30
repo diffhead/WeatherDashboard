d97621743f2eb5e11f6cf90415bba38ccd4153f3
Viktor Smagin <thinlineseverywhere@gmail.com>
2022-05-01 00:15:56 +0300

    Back-end: Cli error codes class implementation

---
+++ Cli/ErrorCode.php
@@ -0,0 +1,9 @@
+<?php namespace Cli;
+
+class ErrorCode
+{
+    public const ERR_ROUTE_NOT_FOUND = 1;
+    public const ERR_ACCESS_DENIED = 2;
+    public const ERR_BAD_REQUEST = 3;
+    public const ERR_HAVE_ERRORS = 4;
+}
