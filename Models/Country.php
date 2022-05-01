aa6a4f0fa7bb45c1573172d9b2833857fb644f94
Viktor Smagin <thinlineseverywhere@gmail.com>
2022-05-01 19:29:35 +0300

    Back-End: Models\Country -> removed itu field, appended getByISO method.

--- Models/Country.php
+++ Models/Country.php
@@ -3,6 +3,8 @@
 use Core\Model;
 use Core\ActiveRecord;
 
+use Services\ArrayService;
+
 class Country extends Model
 {
     protected string $title;
@@ -18,4 +20,15 @@ class Country extends Model
         'title' => ActiveRecord::TYPE_STRING,
         'iso'   => ActiveRecord::TYPE_STRING
     ];
+
+    public static function getByISO(string $iso): array
+    {
+        $country = static::where("iso = '$iso'");
+
+        if ( ArrayService::isEmpty($country) ) {
+            return [];
+        }
+
+        return ArrayService::pop($country);
+    }
 }
