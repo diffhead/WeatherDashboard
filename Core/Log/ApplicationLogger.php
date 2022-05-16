435e90abf6dd3bb3830864a28f50e3579a8f24f0
Viktor Smagin <thinlineseverywhere@gmail.com>
2022-05-16 14:02:45 +0300

    class Core\Log\ApplicationLogger extends AbstractStaticLogger through implements LoggerStatic

--- Core/Log/ApplicationLogger.php
+++ Core/Log/ApplicationLogger.php
@@ -1,40 +1,7 @@
 <?php namespace Core\Log;
 
-use Interfaces\Logger;
-use Interfaces\LoggerStatic;
-
-class ApplicationLogger implements LoggerStatic
+class ApplicationLogger extends AbstractStaticLogger
 {
-    private static ?Logger $instance = null;
-
-    public static function getInstance(): Logger
-    {
-        if ( self::$instance === null ) {
-            self::$instance = new FileLog(
-                _APP_BASE_DIR_ . 'log/application_' . date('dmY') . '.log', E_ALL
-            );
-        }
-
-        return self::$instance;
-    }
-
-    public static function log(string $message, int $level = E_NOTICE): bool
-    {
-        return self::getInstance()->log($message, $level);
-    }
-
-    public static function notice(string $message): bool
-    {
-        return self::getInstance()->notice($message);
-    }
-
-    public static function warning(string $message): bool
-    {
-        return self::getInstance()->warning($message);
-    }
-
-    public static function error(string $message): bool
-    {
-        return self::getInstance()->error($message);
-    }
+    protected static string  $logFile     = 'application';
+    protected static bool    $logFileDate = true;
 }
